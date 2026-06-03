66cd9c38a840c6df03146ccd7b425a040b55cf53
kjp0411 <98109773+kjp0411@users.noreply.github.com>
2026-06-03 20:27:42 +0900

    solve: programmers lv2 p12914 멀리 뛰기

---
+++ src/main/java/programmers/lv2/p12914/Solution.java
@@ -0,0 +1,34 @@
+package programmers.lv2.p12914;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12914
+
+public class Solution {
+    public long solution(int n) {
+        long[] dp = new long[n + 1];
+
+        dp[1] = 1;
+
+        if (n >= 2) {
+            dp[2] = 2;
+        }
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int n1 = 4;
+        int n2 = 3;
+
+        long result1 = solution.solution(n1);
+        long result2 = solution.solution(n2);
+
+        System.out.println(result1); // 5
+        System.out.println(result2); // 3
+    }
+}
